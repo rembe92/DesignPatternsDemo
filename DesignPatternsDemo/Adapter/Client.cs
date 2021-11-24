7c78597c03eefbd6899f674715217d31d05c3452
rembe92 <rembe0096@gmail.com>
2021-11-24 17:00:01 +0100

    implemting parts of the adapter patterns

---
+++ DesignPatternsDemo/Adapter/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatternsDemo.Adapter
+{
+    //Not changes the underlying behavior
+    //Combines two different types of interfaces where the two interfaces are not interoperable
+    //the adapter patterns makes the interfaces interoperable
+    //source https://www.youtube.com/watch?v=2PKQtcJjYvc&t=137s&ab_channel=ChristopherOkhravi
+    internal class Client
+    {
+    }
+}
