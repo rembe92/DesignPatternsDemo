2f2104de1362b2169139d08b89a5ce4cc692aad9
rembe92 <rembe0096@gmail.com>
2021-11-29 17:13:28 +0100

    proxy pattern Client

---
+++ DesignPatternsDemo/Proxy/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatternsDemo.Proxy
+{
+    internal class Client
+    {
+        //provides a placeholder or surrogate control for another object to control access to it
+        public void Demo()
+        {
+
+        }
+    }
+}
